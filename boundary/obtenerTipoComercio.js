6ba0155753102d9d123ca8e2d73aca97c4a18804
Mariana Jeaneth Gochez Santos <GS20027@UES.EDU.SV>
2023-06-23 00:34:17 -0600

    frontend

--- boundary/obtenerTipoComercio.js
+++ boundary/obtenerTipoComercio.js
@@ -1,28 +1,23 @@
-import { clasificarCategorias, ordenarTipoComercios } from "../control/ordenarCategorias.js";
+import { clasificarCategorias,  } from "../control/ordenarCategorias.js";
 
 
 async function getTipoComercios() { 
-    return fetch("http://20.14.165.228:8282/deliveryApp/tipocomercio/all") 
-    .then(response => { 
-      if (response.ok) { 
-        return response.json(); 
-      } else { 
-        throw new Error('Error al obtener los datos del servidor.'); 
-      } 
-    }) 
-    .then(data => { 
-       const categorias =ordenarTipoComercios(data) 
-
-        return categorias; // Devolver los datos obtenidos 
-       
-    }) 
-    .catch(error => { 
-      //console.error(error); 
-    }); 
-} 
+  try {
+    const response = await fetch("http://20.14.165.228:8282/deliveryApp/tipocomercio/all");
+    if (response.ok) { 
+      const data = await response.json();
+      return data; // Devolver los datos obtenidos 
+    } else { 
+      throw new Error('Error al obtener los datos del servidor.'); 
+    } 
+  } catch (error) {
+    //console.error(error);
+    throw error; // Propagar el error hacia arriba
+  }
+}
 
 async function getAllComercios() { 
-  return fetch("http://20.14.165.228:8080/Delivery-1.0.0-SNAPSHOT/comercio_tipocomercio/all") 
+  return fetch("http://20.14.165.228:8282/deliveryApp/comercio_tipocomercio/all") 
   .then(response => { 
     if (response.ok) { 
       return response.json(); 
@@ -40,4 +35,32 @@ async function getAllComercios() {
     console.error(error); 
   }); 
 } 
-export {getTipoComercios,getAllComercios};
+async function getComercioProducto() { 
+  try {
+    const response = await fetch("http://20.14.165.228:8282/deliveryApp/comercio_productos/all");
+    if (response.ok) { 
+      const data = await response.json();
+      return data; // Devolver los datos obtenidos 
+    } else { 
+      throw new Error('Error al obtener los datos del servidor.'); 
+    } 
+  } catch (error) {
+    //console.error(error);
+    throw error; // Propagar el error hacia arriba
+  }
+}
+async function getComercioSucursales() { 
+  try {
+    const response = await fetch("http://20.14.165.228:8282/deliveryApp/comercio_sucursales/all");
+    if (response.ok) { 
+      const data = await response.json();
+      return data; // Devolver los datos obtenidos 
+    } else { 
+      throw new Error('Error al obtener los datos del servidor.'); 
+    } 
+  } catch (error) {
+    //console.error(error);
+    throw error; // Propagar el error hacia arriba
+  }
+}
+export {getTipoComercios,getAllComercios,getComercioProducto,getComercioSucursales};
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import { getTipoComercios } from "./boundary/obtenerTipoComercio.js";
+import { getTipoComercios,getAllComercios,getComercioProducto, getComercioSucursales } from "./boundary/obtenerTipoComercio.js";
+
 mocha.setup('bdd');
 const expect = chai.expect;
 const assert = chai.assert;
@@ -8,10 +9,10 @@ describe('Obtener tipoComercios', function() {
 
     });
 
-    it('debería obtener tipo comercios', function() {
+    it('debería obtener tipo comercios', async function() {
         console.log(getTipoComercios())
 
-        const resultado = getTipoComercios();
+        const resultado = await getTipoComercios();
 
         expect(resultado).to.exist;
     });
@@ -25,5 +26,72 @@ describe('Obtener tipoComercios', function() {
         // Limpiar después de cada prueba
     });
 });
+describe('Deberia obtener todos los comercios', function() {
+    beforeEach(function() {
+
+    });
+
+    it('debería obtener tipo comercios', async function() {
+        console.log(getTipoComercios())
+
+        const resultado = await getAllComercios();
+
+        expect(resultado).to.exist;
+    });
+
+    it('debería hacer algo más', function() {
+        // Otra prueba aquí
+        assert.strictEqual('hola'.length, 4);
+    });
+
+    afterEach(function() {
+        // Limpiar después de cada prueba
+    });
+});
+describe('Obtener comercioProducto', function() {
+    beforeEach(function() {
+
+    });
+
+    it('debería obtener tipo comercios', async function() {
+        console.log(getTipoComercios())
+
+        const resultado = await getComercioProducto();
+
+        expect(resultado).to.exist;
+    });
+
+    it('debería hacer algo más', function() {
+        // Otra prueba aquí
+        assert.strictEqual('hola'.length, 4);
+    });
+
+    afterEach(function() {
+        // Limpiar después de cada prueba
+    });
+});
+describe('Obtener comercioSucursales', function() {
+    beforeEach(function() {
+
+    });
+
+    it('debería obtener tipo comercios', async function() {
+        console.log(getTipoComercios())
+
+        const resultado = await getComercioSucursales();
+
+        expect(resultado).to.exist;
+    });
+
+    it('debería hacer algo más', function() {
+        // Otra prueba aquí
+        assert.strictEqual('hola'.length, 4);
+    });
+
+    afterEach(function() {
+        // Limpiar después de cada prueba
+    });
+});
+
 
 mocha.run();
